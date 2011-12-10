f5e4b789647bade6112f793ba83a9f6b36d73565
Your Name <itsadizasta_skywalkawaafta@yahoo.com>
2011-12-10 14:17:25 -0600

    more clean up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def index
+    @reviews = Review.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @reviews }
+    end
+  end
+
+  # GET /reviews/1
+  # GET /reviews/1.json
+  def show
+    @review = Review.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @review }
+    end
+  end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,26 +1,4 @@
 class ReviewsController < ApplicationController
-  # GET /reviews
-  # GET /reviews.json
-  def index
-    @reviews = Review.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @reviews }
-    end
-  end
-
-  # GET /reviews/1
-  # GET /reviews/1.json
-  def show
-    @review = Review.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @review }
-    end
-  end
-
   # GET /reviews/new
   # GET /reviews/new.json
   def new

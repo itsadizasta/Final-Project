ab2536e9a7fa91fa4b72d7bdfce85d854b341861
Your Name <itsadizasta_skywalkawaafta@yahoo.com>
2011-12-10 12:55:18 -0600

    some cleaning

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-UserReviews::Application.config.session_store :cookie_store, key: '_example_session'
+UserReviews::Application.config.session_store :cookie_store, key: '_userreviews_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
